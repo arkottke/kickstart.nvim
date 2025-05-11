cb2b13d51ad2030856ebccb9d09c224fcf896bc4
Ori Perry <48057913+oriori1703@users.noreply.github.com>
2025-05-11 03:16:03 +0300

    Replace vim.opt with vim.o (#1495)
    * Replace vim.opt with vim.o
    
    Because it offers a nicer interface and info on hover.
    For now leave vim.opt when using the table interface (until vim.o
    with tables is implemented)
    
    * Add type hint for vim.opt.rtp
    
    * Add a comment about using vim.opt instead of vim.o

--- init.lua
+++ init.lua
@@ -175,7 +175,7 @@ vim.o.confirm = true
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
-vim.opt.confirm = true
+vim.o.confirm = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
