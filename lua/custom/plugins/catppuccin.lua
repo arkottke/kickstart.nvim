84bf0107c584f990c8bf45f9ac0f6eec45827a21
Albert Kottke <albert.kottke@gmail.com>
2026-06-15 19:04:40 -0700

    Changed default theme to frappe.

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -1,7 +1,7 @@
 vim.pack.add { { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' } }
 
 require('catppuccin').setup {
-  flavour = 'latte',
+  flavour = 'frappe',
 }
 
-vim.cmd.colorscheme 'catppuccin-latte'
+vim.cmd.colorscheme 'catppuccin-frappe'
